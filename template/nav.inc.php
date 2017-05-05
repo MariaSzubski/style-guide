e6ca4f502dc5d31bdea420496c77da40cb3dc1d2
Maria Szubski <MariaSzubski@gmail.com>
2017-05-05 15:19:53 -0400

    Add collpasible navigation. Revise Reporting Palette.

--- template/nav.inc.php
+++ template/nav.inc.php
@@ -3,77 +3,90 @@
 		<h1>Nielsen Experience UI Guidelines</h1>
 		<h3>V8.0 &ndash; Updated March 31, 2017</h3>
 	</header>
-	<h4>About the Guidelines</h4>
-	<ul>
-		<li class="<?php echo ($current == "index.php" ? "selected" : "")?>"><a href="index.php">Introduction</a></li>
-		<li class="<?php echo ($current == "changelog.php" ? "selected" : "")?>"><a href="changelog.php">Changelog</a></li>
-		<li class="<?php echo ($current == "usage.php" ? "selected" : "")?>"><a href="usage.php">Usage</a></li>
-		<li class="<?php echo ($current == "contact_us.php" ? "selected" : "")?>"><a href="contact_us.php">Contact Us</a></li>
-	</ul>
+
+	<details open>
+		<summary><span>About the Guidelines</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "index.php" ? "selected" : "")?>"><a href="index.php">Introduction</a></li>
+			<li class="<?php echo ($current == "changelog.php" ? "selected" : "")?>"><a href="changelog.php">Changelog</a></li>
+			<li class="<?php echo ($current == "usage.php" ? "selected" : "")?>"><a href="usage.php">Usage</a></li>
+			<li class="<?php echo ($current == "contact_us.php" ? "selected" : "")?>"><a href="contact_us.php">Contact Us</a></li>
+		</ul>
+	</details>
 	<!-- -->
-	<h4>Components</h4>
-	<ul>
-		<li class="<?php echo ($current == "button.php" ? "selected" : "")?>"><a href="button.php">Button</a></li>
-		<li class="<?php echo ($current == "card.php" ? "selected" : "")?>"><a href="card.php">Card</a></li>
-		<li class="<?php echo ($current == "checkbox.php" ? "selected" : "")?>"><a href="checkbox.php">Checkbox</a></li>
-		<li class="<?php echo ($current == "chips.php" ? "selected" : "")?>"><a href="chips.php">Chips</a></li>
-		<li class="<?php echo ($current == "colors.php" ? "selected" : "")?>"><a href="colors.php">Colors</a></li>
-		<li class="<?php echo ($current == "content.php" ? "selected" : "")?>"><a href="content.php">Content</a></li>
-		<li class="<?php echo ($current == "date_picker.php" ? "selected" : "")?>"><a href="date_picker.php">Date Picker</a></li>
-		<li class="<?php echo ($current == "dialog.php" ? "selected" : "")?>"><a href="dialog.php">Dialog</a></li>
-		<li class="<?php echo ($current == "divider.php" ? "selected" : "")?>"><a href="divider.php">Divider</a></li>
-		<li class="<?php echo ($current == "grid_list.php" ? "selected" : "")?>"><a href="grid_list.php">Grid List</a></li>
-		<li class="<?php echo ($current == "icon.php" ? "selected" : "")?>"><a href="icon.php">Icon</a></li>
-		<li class="<?php echo ($current == "input.php" ? "selected" : "")?>"><a href="input.php">Input</a></li>
-		<li class="<?php echo ($current == "list.php" ? "selected" : "")?>"><a href="list.php">List</a></li>
-		<li class="<?php echo ($current == "menu.php" ? "selected" : "")?>"><a href="menu.php">Menu</a></li>
-		<li class="<?php echo ($current == "menu_bar.php" ? "selected" : "")?>"><a href="menu_bar.php">Menu Bar</a></li>
-		<li class="<?php echo ($current == "nav_bar.php" ? "selected" : "")?>"><a href="nav_bar.php">Nav Bar</a></li>
-		<li class="<?php echo ($current == "panel.php" ? "selected" : "")?>"><a href="panel.php">Panel</a></li>
-		<li class="<?php echo ($current == "progress_circular.php" ? "selected" : "")?>"><a href="progress_circular.php">Progress Circular</a></li>
-		<li class="<?php echo ($current == "progress_linear.php" ? "selected" : "")?>"><a href="progress_linear.php">Progress Linear</a></li>
-		<li class="<?php echo ($current == "radio_button.php" ? "selected" : "")?>"><a href="radio_button.php">Radio Button</a></li>
-		<li class="<?php echo ($current == "select.php" ? "selected" : "")?>"><a href="select.php">Select</a></li>
-		<li class="<?php echo ($current == "sidenav_left.php" ? "selected" : "")?>"><a href="sidenav_left.php">Sidenav (Left)</a></li>
-		<li class="<?php echo ($current == "sidenav_right.php" ? "selected" : "")?>"><a href="sidenav_right.php">Sidenav (Right)</a></li>
-		<li class="<?php echo ($current == "switch.php" ? "selected" : "")?>"><a href="switch.php">Switch</a></li>
-		<li class="<?php echo ($current == "tabs.php" ? "selected" : "")?>"><a href="tabs.php">Tabs</a></li>
-		<li class="<?php echo ($current == "toast.php" ? "selected" : "")?>"><a href="toast.php">Toast</a></li>
-		<li class="<?php echo ($current == "toolbar.php" ? "selected" : "")?>"><a href="toolbar.php">Toolbar</a></li>
-		<li class="<?php echo ($current == "tooltip.php" ? "selected" : "")?>"><a href="tooltip.php">Tooltip</a></li>
-		<li class="<?php echo ($current == "whiteframe.php" ? "selected" : "")?>"><a href="whiteframe.php">Whiteframe</a></li>
-	</ul>
+	<details open>
+		<summary><span>Components</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "button.php" ? "selected" : "")?>"><a href="button.php">Button</a></li>
+			<li class="<?php echo ($current == "card.php" ? "selected" : "")?>"><a href="card.php">Card</a></li>
+			<li class="<?php echo ($current == "checkbox.php" ? "selected" : "")?>"><a href="checkbox.php">Checkbox</a></li>
+			<li class="<?php echo ($current == "chips.php" ? "selected" : "")?>"><a href="chips.php">Chips</a></li>
+			<li class="<?php echo ($current == "colors.php" ? "selected" : "")?>"><a href="colors.php">Colors</a></li>
+			<li class="<?php echo ($current == "content.php" ? "selected" : "")?>"><a href="content.php">Content</a></li>
+			<li class="<?php echo ($current == "date_picker.php" ? "selected" : "")?>"><a href="date_picker.php">Date Picker</a></li>
+			<li class="<?php echo ($current == "dialog.php" ? "selected" : "")?>"><a href="dialog.php">Dialog</a></li>
+			<li class="<?php echo ($current == "divider.php" ? "selected" : "")?>"><a href="divider.php">Divider</a></li>
+			<li class="<?php echo ($current == "grid_list.php" ? "selected" : "")?>"><a href="grid_list.php">Grid List</a></li>
+			<li class="<?php echo ($current == "icon.php" ? "selected" : "")?>"><a href="icon.php">Icon</a></li>
+			<li class="<?php echo ($current == "input.php" ? "selected" : "")?>"><a href="input.php">Input</a></li>
+			<li class="<?php echo ($current == "list.php" ? "selected" : "")?>"><a href="list.php">List</a></li>
+			<li class="<?php echo ($current == "menu.php" ? "selected" : "")?>"><a href="menu.php">Menu</a></li>
+			<li class="<?php echo ($current == "menu_bar.php" ? "selected" : "")?>"><a href="menu_bar.php">Menu Bar</a></li>
+			<li class="<?php echo ($current == "nav_bar.php" ? "selected" : "")?>"><a href="nav_bar.php">Nav Bar</a></li>
+			<li class="<?php echo ($current == "panel.php" ? "selected" : "")?>"><a href="panel.php">Panel</a></li>
+			<li class="<?php echo ($current == "progress_circular.php" ? "selected" : "")?>"><a href="progress_circular.php">Progress Circular</a></li>
+			<li class="<?php echo ($current == "progress_linear.php" ? "selected" : "")?>"><a href="progress_linear.php">Progress Linear</a></li>
+			<li class="<?php echo ($current == "radio_button.php" ? "selected" : "")?>"><a href="radio_button.php">Radio Button</a></li>
+			<li class="<?php echo ($current == "select.php" ? "selected" : "")?>"><a href="select.php">Select</a></li>
+			<li class="<?php echo ($current == "sidenav_left.php" ? "selected" : "")?>"><a href="sidenav_left.php">Sidenav (Left)</a></li>
+			<li class="<?php echo ($current == "sidenav_right.php" ? "selected" : "")?>"><a href="sidenav_right.php">Sidenav (Right)</a></li>
+			<li class="<?php echo ($current == "switch.php" ? "selected" : "")?>"><a href="switch.php">Switch</a></li>
+			<li class="<?php echo ($current == "tabs.php" ? "selected" : "")?>"><a href="tabs.php">Tabs</a></li>
+			<li class="<?php echo ($current == "toast.php" ? "selected" : "")?>"><a href="toast.php">Toast</a></li>
+			<li class="<?php echo ($current == "toolbar.php" ? "selected" : "")?>"><a href="toolbar.php">Toolbar</a></li>
+			<li class="<?php echo ($current == "tooltip.php" ? "selected" : "")?>"><a href="tooltip.php">Tooltip</a></li>
+			<li class="<?php echo ($current == "whiteframe.php" ? "selected" : "")?>"><a href="whiteframe.php">Whiteframe</a></li>
+		</ul>
+	</details>
 	<!-- -->
-	<h4>Custom Components</h4>
-	<ul>
-		<li class="<?php echo ($current == "app_bar.php" ? "selected" : "")?>"><a href="app_bar.php">App Bar</a></li>
-		<li class="<?php echo ($current == "charts.php" ? "selected" : "")?>"><a href="charts.php">Charts</a></li>
-		<li class="<?php echo ($current == "copyright.php" ? "selected" : "")?>"><a href="copyright.php">Copyright</a></li>
-		<li class="<?php echo ($current == "grid.php" ? "selected" : "")?>"><a href="grid.php">Grid</a></li>
-		<li class="<?php echo ($current == "pagination.php" ? "selected" : "")?>"><a href="pagination.php">Pagination</a></li>
-		<li class="<?php echo ($current == "reporting_palette.php" ? "selected" : "")?>"><a href="reporting_palette.php">Reporting Palette</a></li>
-		<li class="<?php echo ($current == "wizard.php" ? "selected" : "")?>"><a href="wizard.php">Wizard</a></li>
-	</ul>
+	<details open>
+		<summary><span>Patterns</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "app_bar.php" ? "selected" : "")?>"><a href="app_bar.php">App Bar</a></li>
+			<li class="<?php echo ($current == "charts.php" ? "selected" : "")?>"><a href="charts.php">Charts</a></li>
+			<li class="<?php echo ($current == "copyright.php" ? "selected" : "")?>"><a href="copyright.php">Copyright</a></li>
+			<li class="<?php echo ($current == "grid.php" ? "selected" : "")?>"><a href="grid.php">Grid</a></li>
+			<li class="<?php echo ($current == "pagination.php" ? "selected" : "")?>"><a href="pagination.php">Pagination</a></li>
+			<li class="<?php echo ($current == "reporting_palette.php" ? "selected" : "")?>"><a href="reporting_palette.php">Reporting Palette</a></li>
+			<li class="<?php echo ($current == "wizard.php" ? "selected" : "")?>"><a href="wizard.php">Wizard</a></li>
+		</ul>
+	</details>
 	<!-- -->
-	<h4>Typography</h4>
-	<ul>
-		<li class="<?php echo ($current == "type_styles.php" ? "selected" : "")?>"><a href="type_styles.php">Type Styles</a></li>
-	</ul>
+	<details open>
+		<summary><span>Typography</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "type_styles.php" ? "selected" : "")?>"><a href="type_styles.php">Type Styles</a></li>
+		</ul>
+	</details>
 	<!-- -->
-	<h4>Iconography</h4>
-	<ul>
-		<li class="<?php echo ($current == "i_app_bar.php" ? "selected" : "")?>"><a href="i_app_bar.php">App Bar</a></li>
-		<li class="<?php echo ($current == "i_common_actions.php" ? "selected" : "")?>"><a href="i_common_actions.php">Common Actions</a></li>
-		<li class="<?php echo ($current == "i_communication.php" ? "selected" : "")?>"><a href="i_communication.php">Communication</a></li>
-		<li class="<?php echo ($current == "i_data_report_types.php" ? "selected" : "")?>"><a href="i_data_report_types.php">Data &amp; Report Types</a></li>
-		<li class="<?php echo ($current == "i_toolbar.php" ? "selected" : "")?>"><a href="i_toolbar.php">Toolbar</a></li>
-		<li class="<?php echo ($current == "i_ui_component_controls.php" ? "selected" : "")?>"><a href="i_ui_component_controls.php">UI Component Controls</a></li>
-		<li class="<?php echo ($current == "i_view_controls.php" ? "selected" : "")?>"><a href="i_view_controls.php">View Controls</a></li>
-	</ul>
+	<details open>
+		<summary><span>Iconography</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "i_app_bar.php" ? "selected" : "")?>"><a href="i_app_bar.php">App Bar</a></li>
+			<li class="<?php echo ($current == "i_common_actions.php" ? "selected" : "")?>"><a href="i_common_actions.php">Common Actions</a></li>
+			<li class="<?php echo ($current == "i_communication.php" ? "selected" : "")?>"><a href="i_communication.php">Communication</a></li>
+			<li class="<?php echo ($current == "i_data_report_types.php" ? "selected" : "")?>"><a href="i_data_report_types.php">Data &amp; Report Types</a></li>
+			<li class="<?php echo ($current == "i_toolbar.php" ? "selected" : "")?>"><a href="i_toolbar.php">Toolbar</a></li>
+			<li class="<?php echo ($current == "i_ui_component_controls.php" ? "selected" : "")?>"><a href="i_ui_component_controls.php">UI Component Controls</a></li>
+			<li class="<?php echo ($current == "i_view_controls.php" ? "selected" : "")?>"><a href="i_view_controls.php">View Controls</a></li>
+		</ul>
+	</details>
 	<!-- -->
-	<h4>Examples</h4>
-	<ul>
-		<li class="<?php echo ($current == "ex_multi-panel.php" ? "selected" : "")?>"><a href="ex_multi-panel.php">Multi-Panel Layout</a></li>
-		<li class="<?php echo ($current == "ex_single-panel.php" ? "selected" : "")?>"><a href="ex_single-panel.php">Single Panel Layout</a></li>
-	</ul>
+	<details open>
+		<summary><span>Examples</span></summary>
+		<ul>
+			<li class="<?php echo ($current == "ex_multi-panel.php" ? "selected" : "")?>"><a href="ex_multi-panel.php">Multi-Panel Layout</a></li>
+			<li class="<?php echo ($current == "ex_single-panel.php" ? "selected" : "")?>"><a href="ex_single-panel.php">Single Panel Layout</a></li>
+		</ul>
+	</details>
 </nav>
