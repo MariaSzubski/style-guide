cf830ebb56c98716735b07268c3bfcdce61269f2
Maria Szubski <MariaSzubski@gmail.com>
2017-07-12 16:06:10 -0400

    Create Pattern: Empty States page.

---
+++ p_empty_states.php
@@ -0,0 +1,92 @@
+<?php $current = array_pop(explode("/", $_SERVER['PHP_SELF'])); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title id="title">NX Guidelines - Empty States</title>
+	<link rel="stylesheet" href="css/build/style_v8-2.css">
+</head>
+<body>
+	<main class="default_state">
+		<header>
+			<h2 id="pg_header">Empty States</h2>
+			<div class="btn-group" role="group">
+				Units:
+				<button type="button" id="size-px" class="btn-selected">px</button><button type="button" id="size-rem">rem</button>
+			</div>
+			<img src="img/hamburger.svg" alt="Navigation" id="icon_nav">
+			<img src="img/n_tab.svg" alt="Nielsen" id="icon_ntab">
+		</header>
+		<article id="guidelines">
+			<div id="container">
+				<!-- Section 1 -->
+				<section class="full">
+					<h2>Large</h2>
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_01.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_02.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_03.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_04.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_05.png" alt="Label">
+					</section>
+				</section>
+				<!-- Section 2 -->
+				<section class="full">
+					<h2>Medium</h2>
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_06.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_07.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_08.png" alt="Label">
+					</section>
+				</section>
+				<!-- Section 3 -->
+				<section class="full">
+					<h2>Small</h2>
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_09.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_10.png" alt="Label">
+					</section>
+					<!-- -->
+					<section class="full">
+						<h4><!--Pattern Title--></h4>
+						<img src="img/guide/p_empty_state_11.png" alt="Label">
+					</section>
+				</section>
+			</div>
+		</article>
+	</main>
+	<?php require 'template/nav.inc.php'; ?>
+	<?php require 'template/scripts.inc.php'; ?>
+</body>
+</html>
--- template/nav.inc.php
+++ template/nav.inc.php
@@ -67,6 +67,7 @@
 			<li class="<?php echo ($current == "p_copyright.php" ? "selected" : "")?>"><a href="p_copyright.php">Copyright</a></li>
 			<li class="<?php echo ($current == "p_data_context_picker.php" ? "selected" : "")?>"><a href="p_data_context_picker.php">Data Context Picker</a></li>
 			<li class="<?php echo ($current == "p_dialog.php" ? "selected" : "")?>"><a href="p_dialog.php">Dialog</a></li>
+			<li class="<?php echo ($current == "p_empty_states.php" ? "selected" : "")?>"><a href="p_empty_states.php">Empty States</a></li>
 			<li class="<?php echo ($current == "p_grid.php" ? "selected" : "")?>"><a href="p_grid.php">Grid</a></li>
 			<li class="<?php echo ($current == "p_pagination.php" ? "selected" : "")?>"><a href="p_pagination.php">Pagination</a></li>
 			<li class="<?php echo ($current == "p_panel.php" ? "selected" : "")?>"><a href="p_panel.php">Panel</a></li>
